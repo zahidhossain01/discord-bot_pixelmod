e426a444a887a9d925b6429208c39571cad1d213
Zahid Hossain <zpangolin@outlook.com>
2023-10-08 13:17:46 -0500

    shutdown command

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+# https://discord.com/api/oauth2/authorize?client_id=1160302892729774190&permissions=139586750528&scope=bot
+
 import discord
+from discord.ext import commands
 
 import PIL.Image as Image
 import numpy as np
@@ -7,19 +10,27 @@ import pixelmod
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"We have logged in as {bot.user}")
 
-@client.event
-async def on_message(msg):
-    if msg.author == client.user:
-        return
-    
-    if msg.content.startswith('$poop'):
-        await msg.channel.send('pee!')
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("bye bye :c")
+    await bot.close()
 
+
+
+@bot.command()
+async def poop(ctx):
+    print("pee!")
+    await ctx.send('pee!')
+
+##################################
 TOKEN = 'MTE2MDMwMjg5MjcyOTc3NDE5MA.Gh5Kre.P1OwCPqxx1deUh4Iyj9sMBOotEZX1gPNEAfgwU'
-client.run(TOKEN)
+bot.run(TOKEN)
