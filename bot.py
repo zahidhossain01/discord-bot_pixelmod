ce17be9d2150bf905a434b7723ca7332f5b775b7
Zahid Hossain <zpangolin@outlook.com>
2023-10-10 13:55:28 -0500

    image send success

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
-# https://discord.com/api/oauth2/authorize?client_id=1160302892729774190&permissions=139586750528&scope=bot
+# command syncing notes for slash commands:
+# https://gist.github.com/AbstractUmbra/a9c188797ae194e592efe05fa129c57f
+# https://discordpy.readthedocs.io/en/latest/interactions/api.html#discord.app_commands.CommandTree.copy_global_to
+
+# consider discord.js instead of discord.py some day?
+
+# https://stackoverflow.com/questions/52241051/i-want-to-let-my-discord-bot-send-images-gifs
 
 import discord
 from discord.ext import commands
-from discord import app_commands
 
 import PIL.Image as Image
 import numpy as np
@@ -11,17 +16,15 @@ import pixelmod
 import os
 from dotenv import load_dotenv
 
-
 intents = discord.Intents.default()
 intents.message_content = True
-
-# client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
-# TODO: real slash commands, not this ^
+
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
+    await bot.tree.sync()
 
 @bot.command()
 @commands.is_owner()
@@ -29,32 +32,28 @@ async def shutdown(ctx):
     await ctx.send("bye bye :c")
     await bot.close()
 
-
-
-# @bot.command()
-# async def poop(ctx):
-#     print("pee!")
-#     await ctx.send('pee!')
-
-# TODO: need to sync commands to guild? how to properly do it... different when local testing vs deployed?
-# technically only need to sync once...? can bot sync upon being added to server? how to resync while it's deployed (if even possible)?
-# https://gist.github.com/AbstractUmbra/a9c188797ae194e592efe05fa129c57f
-# https://discordpy.readthedocs.io/en/latest/interactions/api.html#discord.app_commands.CommandTree.copy_global_to
-
-# @bot.command()
-# async def sync(ctx):
-#     guild = ctx.guild
-#     bot.tree.copy_global_to(guild=guild) # this apparently just copies, still need to sync?
-
-@bot.event
-async def on_ready():
-    await bot.tree.sync()
-
-
 @bot.tree.command()
 async def poop(interaction:discord.Interaction):
-    print("pee!")
-    await interaction.response.send_message("pee!", ephemeral=True)
+    await interaction.response.send_message("pee!")
+
+def create_image():
+    # 100x100
+    I = np.full((100,100,3), 128).astype(np.uint8)
+    I[0:25,0:25,:] = 0
+    I[75:100,75:100,:] = 255
+    img = Image.fromarray(I)
+    img.save("testimage.jpg", quality=95, subsampling=0)
+    return "testimage.jpg"
+
+@bot.command()
+async def createimage(ctx):
+    filepath = create_image()
+    # await ctx.send(file=discord.File(filepath))
+    with open(filepath, 'rb') as f:
+        img = discord.File(f)
+        await ctx.send(file=img)
+    
+
 
 
 
