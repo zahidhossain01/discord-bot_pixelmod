7ed3c99d245046ccb63ad22cb71d9a0a1d66c520
Zahid Hossain <zpangolin@outlook.com>
2023-10-10 01:10:58 -0500

    failed attempts at slash cmd

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 import PIL.Image as Image
 import numpy as np
@@ -16,7 +17,7 @@ intents.message_content = True
 
 # client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
-
+# TODO: real slash commands, not this ^
 
 @bot.event
 async def on_ready():
@@ -30,10 +31,32 @@ async def shutdown(ctx):
 
 
 
-@bot.command()
-async def poop(ctx):
+# @bot.command()
+# async def poop(ctx):
+#     print("pee!")
+#     await ctx.send('pee!')
+
+# TODO: need to sync commands to guild? how to properly do it... different when local testing vs deployed?
+# technically only need to sync once...? can bot sync upon being added to server? how to resync while it's deployed (if even possible)?
+# https://gist.github.com/AbstractUmbra/a9c188797ae194e592efe05fa129c57f
+# https://discordpy.readthedocs.io/en/latest/interactions/api.html#discord.app_commands.CommandTree.copy_global_to
+
+# @bot.command()
+# async def sync(ctx):
+#     guild = ctx.guild
+#     bot.tree.copy_global_to(guild=guild) # this apparently just copies, still need to sync?
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+
+
+@bot.tree.command()
+async def poop(interaction:discord.Interaction):
     print("pee!")
-    await ctx.send('pee!')
+    await interaction.response.send_message("pee!", ephemeral=True)
+
+
 
 ##################################
 
